bac916b1c398fdf3465422ae8b66636733aa05fa
avafal <ava@firstaccesslending.com>
2026-02-11 09:23:28 -0800

    Add Tailwind and PostCSS config files

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // First Access Lending Blue
+        primary: {
+          light: '#0EF0F0',
+          DEFAULT: '#0283DB',
+          dark: '#003961',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
